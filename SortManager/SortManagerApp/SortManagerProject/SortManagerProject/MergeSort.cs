a0efdb63c44480c8466bc73e6b39460645537266
EllisWitten <e.witten.2001@gmail.com>
2022-07-20 17:20:03 +0100

    readme template

---
+++ SortManager/SortManagerApp/SortManagerProject/SortManagerProject/MergeSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortManagerProject
+{
+    public class MergeSort
+    {
+        public static int[] MergeSortRecursive(int[] input)
+        {
+            if (input.Length == 1)
+                return input;
+
+            int midpoint = (input.Length / 2);
+            int[] left = new int[midpoint];
+            
+            for (int i = 0; i < midpoint; i++)
+                left[i] = input[i];
+
+            int[] right = new int[midpoint];
+            for (int i = midpoint; i < input.Length; i++)
+                right[i] = input[i];
+        }
+
+        private static void Merge(int[] input, int left, int m, int right)
+        {
+
+        }
+    }
+}
