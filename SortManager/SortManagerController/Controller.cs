eb82eba4fe6b2e18ad3e63d2e23b04e65b224a62
JonOfScience <jdcrofts31@gmail.com>
2022-07-21 15:39:22 +0100

    Controller Initial Structure

---
+++ SortManager/SortManagerController/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SortManagerModel;
+namespace SortManagerController
+{
+    public enum Algorithms { MERGESORT }
+    public class Controller : ISort
+    {
+        public Algorithms Algorithm { get; set; }
+
+        public Controller(string sortMethod)
+        {
+            switch (sortMethod.ToLower())
+            {
+                case "mergesort":
+                    Algorithm = Algorithms.MERGESORT;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Sort algorithm is unrecognised");
+            }
+        }
+
+        public int[] Sort(int[] input)
+        {
+            switch (Algorithm)
+            {
+                case Algorithms.MERGESORT:
+                    var _sorter = new MergeSort();
+                    return _sorter.Sort(input);
+                default:
+                    break;
+            }
+        }
+    }
+}
